3cd5da0ff0b295930235dbf5278d1ae3925c3a36
U$6Y5w*q7LPfGTvbnxE2Mn5k <97105947@st.deltion.nl>
2024-09-12 20:24:41 +0200

    Opdracht 2.3 V2

---
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3_v2.py
@@ -0,0 +1,18 @@
+# Test code door AI geschreven, Kijken hoe deze code werkt 
+# en deze werkend toepassen
+tree = [
+    "    *",
+    "   ***",
+    "  ******",
+    " ********",
+    "***********",
+    "    ***",
+    "    ***",
+    "    ***"
+]
+
+for line in tree:
+    print((line + " ") * 5)
+
+# Dit zorgt nog steeds voor geen ruimte tussen de kerstbomen 
+# zonder er extra spaties er aan toe te voegen
