2b811a5982f661edac29583f5017fc6243bbc412
U$6Y5w*q7LPfGTvbnxE2Mn5k <97105947@st.deltion.nl>
2024-09-12 20:21:41 +0200

    Opdracht 3.3 (Controleren)

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,14 @@
 # Groep:
 
 # Hier komt je code...
+while True:
+    aantal = int(input("Hoeveel Kerstbomen wil je op je scherm zien?"))
+
+    print("    *" * aantal)
+    print("   ***" * aantal)
+    print("  ******" * aantal)
+    print(" ********" * aantal)
+    print("***********" * aantal)
+    print("    ***" * aantal)
+    print("    ***" * aantal)
+    print("    ***" * aantal)
