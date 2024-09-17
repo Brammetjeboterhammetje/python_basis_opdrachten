c33ac86f5c0048c5914606bfeaf7c32572d72d3b
U$6Y5w*q7LPfGTvbnxE2Mn5k <97105947@st.deltion.nl>
2024-09-17 09:29:59 +0200

    Geen idee ben de tel kwijt

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,15 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = []
+dict_1 = {"id": 0, "voornaam":"Bram", "Achternaam":"Pierik"}
+dict_2 = {"id": 1, "voornaam":"Jan", "Achternaam":"Jansen"}
+dict_3 = {"id": 2, "voornaam":"Kees", "Achternaam":"Klaassen"}
+dict_4 = {"id": 3, "voornaam":"Piet", "Achternaam":"Pietersen"}
+
+mylist.append(dict_1)
+mylist.append(dict_2)
+mylist.append(dict_3)
+mylist.append(dict_4)
+
+print(mylist[1]["voornaam"]), print(mylist[1]["Achternaam"])
