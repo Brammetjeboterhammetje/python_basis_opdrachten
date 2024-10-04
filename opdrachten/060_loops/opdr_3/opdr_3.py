0b9cf8b9ec81010828acfa2835b482431729ea5c
U$6Y5w*q7LPfGTvbnxE2Mn5k <97105947@st.deltion.nl>
2024-10-04 10:27:49 +0200

    sync

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -6,4 +6,17 @@
 
 # Hier start de for-loop
 
-my_list = []
+# Create an empty list to store the results
+results = []
+
+# Loop from 3 to 81, incrementing by 3
+for i in range(3, 81, 3):
+    # Square the number
+    squared = i ** 2
+    # Divide the squared number by 3
+    result = squared / 3
+    # Append the result to the list
+    results.append(result)
+
+# Print the results
+print(results)
